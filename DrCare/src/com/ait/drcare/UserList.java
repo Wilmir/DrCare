643098d3f095e4f3d32c29d2e56e0e4a4fc4accb
Fiona <A00278892@student.ait.ie>
2020-08-18 20:15:23 +0100

    Added UserList

---
+++ DrCare/src/com/ait/drcare/UserList.java
@@ -0,0 +1,16 @@
+package com.ait.drcare;
+
+import java.util.ArrayList;
+
+public class UserList {
+	//string placeholder until user class is created
+	private ArrayList<String> users;
+
+	public ArrayList<String> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<String> users) {
+		this.users = users;
+	}
+}
