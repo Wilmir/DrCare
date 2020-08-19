21d7fedcff06c3fb354a9d1b1b94ac873fc718a9
Wilmir Nicanor <wbnicanor01@gmail.com>
2020-08-19 17:24:46 +0100

    initialize user bean and registration controller

---
+++ DrCare/src/com/ait/drcare/managedbeans/UserBean.java
@@ -0,0 +1,54 @@
+package com.ait.drcare.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class UserBean {
+	private String name;
+	private String email;
+	private String password;
+	private String passwordConfirmation;
+	private String role;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+	
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
---
+++ DrCare/src/com/ait/drcare/managedbeans/controller/RegistrationController.java
@@ -0,0 +1,57 @@
+package com.ait.drcare.managedbeans.controller;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+import com.ait.drcare.managedbeans.UserBean;
+
+@ManagedBean
+@RequestScoped
+public class RegistrationController {
+
+	@ManagedProperty(value = "#{studentBean}")
+	private UserBean userBean;
+	
+	
+	// 1. Add a User object as a member variable here
+	
+	
+	@PostConstruct
+	public void init() {
+		// 1. Instantiate the user
+	}
+
+	public UserBean getUserBean() {
+		return userBean;
+	}
+
+	public void setUserBean(UserBean userBean) {
+		this.userBean = userBean;
+	}
+	
+	public String addUser() {
+		// 1. Add the attributes from the userBean to the User object
+		
+		
+		// 2. Check if user is not existing
+		// 2.a Verify the user
+		
+		
+		// 3. If user is existing, return a message "User already exists"
+		
+		
+		return "Notification for registration status.";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
