65b4cd3204cecc28644cff860cb0a75d47810d3e
Fiona <A00278892@student.ait.ie>
2020-08-19 11:33:42 +0100

    Added addUser and userExists methods

--- DrCare/src/com/ait/drcare/UserList.java
+++ DrCare/src/com/ait/drcare/UserList.java
@@ -13,4 +13,18 @@ public class UserList {
 	public void setUsers(ArrayList<String> users) {
 		this.users = users;
 	}
+	
+	public void addUser(String user) {
+		this.users.add(user);
+	}
+	
+	public boolean userExists(String user) {
+		boolean exists = false;
+		for (String u : users) {
+			if(u.equals(user)) {
+				exists = true;
+			}
+		}
+		return exists;
+	}
 }
