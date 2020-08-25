91e648825bd66bdc66f090eb8a2d571d1725f1d7
Wilmir <wbnicanor01@gmail.com>
2020-08-25 19:28:24 +0100

    "Refactored RegistrationController for ease of testing"

--- DrCare/src/com/ait/drcare/managedbeans/controller/RegistrationController.java
+++ DrCare/src/com/ait/drcare/managedbeans/controller/RegistrationController.java
@@ -20,73 +20,45 @@ import com.ait.drcare.model.User;
 @RequestScoped
 public class RegistrationController {
 	
-	private UserBean userBean = Helper.getBean("userBean", UserBean.class);
-	
-	private UserListBean registry = Helper.getBean("userListBean", UserListBean.class);
-
-	private User user;
+	private UserListBean registry;
 		
 	@PostConstruct
 	public void init() {
-		// instantiate a user depending on the roles bean type
-		switch(userBean.getRole()) {
-			case "GP":
-				user = new Doctor();
-				break;
-			case "Pharmacist":
-				user = new Pharmacist();
-				break;
-			default:
-				user = new Patient();
-				break;
-		}
+		registry = Helper.getBean("userListBean", UserListBean.class);
 	}
 
-	public UserBean getUserBean() {
-		return userBean;
-	}
-
-	public void setUserBean(UserBean userBean) {
-		this.userBean = userBean;
-	}
 	
-
-	public String addUser() {
+	public String addUser(UserBean userBean) {
 		// 1. Check if the passwords entered match
-		if(!passwordsMatch()){
+		if(!passwordsMatch(userBean)){
 			error("The passwords entered do not match");
 			return null;
 		}
 		
 		// 2. Check if user is not existing		
-		if(isExistingUser()) {
+		if(isExistingUser(userBean)) {
 				error("The credentials you entered already belongs to an existing user");
 				return null;
 		}
 		
 		//3.a Verify the GP
-		if(user.getRole().equals("Doctor") && !isVerifiedGP()){
+		if(userBean.getRole().equals("Doctor") && !isVerifiedGP(userBean)){
 			System.out.println("GP Verification failed. Valid License Number ranges from 300000000 to 300090000");
 			error("We are not able to verify your GP qualification");
 			return null;
 		}
 		
 		//3.b Verify Pharmacist
-		if(user.getRole().equals("Pharmacist") && !isVerifiedPharmacist()){
+		if(userBean.getRole().equals("Pharmacist") && !isVerifiedPharmacist(userBean)){
 			System.out.println("Pharmacist Verification failed. Valid License Number ranges from 600000000 to 600090000");
 			error("We are not able to verify your Pharmacist qualification");
 			return null;
 		}
 		
 
-		// 4. Add the attributes from the userBean to the User object
-		addUserAttributes();
-		
-		
-		//5. Add the new user to the User's List
-		registry.addUser(user);
-		
-		
+		// 4. Add the attributes from the userBean to a new User object
+		addToRegistry(userBean);
+	
 		/*success("Registration successul" + "&nbsp;<a href='login.jsf'>Login</a>");*/
 		
 		System.out.println("New List of Users");
@@ -96,20 +68,18 @@ public class RegistrationController {
 		}
 		
 		
-		//6. Redirect the user to Login page, faces-redirect changes the URl to login.jsf
+		//5. Redirect the user to Login page, faces-redirect changes the URl to login.jsf
 		return "login?faces-redirect=true";
 	}
-		
-	
-	
+
 	// Password validation
-	private boolean passwordsMatch() {
+	private boolean passwordsMatch(UserBean userBean) {
 		return userBean.getPassword().equals(userBean.getPasswordConfirmation());
 	}
 	
 	
 	// Existing user validation
-	private boolean isExistingUser() {
+	private boolean isExistingUser(UserBean userBean) {
 		for(User existingUser : registry.getUsers()) {
 			if(existingUser.getTheEmail().equalsIgnoreCase(userBean.getEmail())) {
 				return true;
@@ -122,18 +92,34 @@ public class RegistrationController {
 	}
 	
 	// GP verification
-	private boolean isVerifiedGP() {
+	private boolean isVerifiedGP(UserBean userBean) {
 		return userBean.getLicenseNumber() >= 300000000 && userBean.getLicenseNumber() <= 399999999;
 	}
 	
 	// Pharmacist verification
-	private boolean isVerifiedPharmacist() {
+	private boolean isVerifiedPharmacist(UserBean userBean) {
 		return userBean.getLicenseNumber() >= 600000000 && userBean.getLicenseNumber() <= 699999999;	
 	}
 	
-	
-	// Add userBean attributes to user instance
-	private void addUserAttributes() {
+	// Add the userBean to the list of registered users
+	private void addToRegistry(UserBean userBean) {
+		// Declare a new user object
+		User user;
+				
+		// instantiate a user depending on the role
+		switch(userBean.getRole()) {
+			case "GP":
+				user = new Doctor();
+				break;
+			case "Pharmacist":
+				user = new Pharmacist();
+				break;
+			default:
+				user = new Patient();
+				break;
+		}
+				
+		// pass the remaining attributes of userBean to the user object
 		user.setThePassword(userBean.getPassword());
 		user.setTheName(userBean.getName());
 		user.setTheEmail(userBean.getEmail());
@@ -147,14 +133,25 @@ public class RegistrationController {
 			for(int i = users.size()-1; i >= 0; i--) {
 				if(registry.getUsers().get(i).getRole().equals("Patient")) {
 					user.setTheID(registry.getUsers().get(i).getTheID() + 1);
-					return;
+					break;
 				}
 			}
 		}else {
 			user.setTheID(userBean.getLicenseNumber());
 		}
+				
+		// Add the new user to the User's List
+		registry.addUser(user);
 	}
 	
+	public UserListBean getRegistry() {
+		return registry;
+	}
+
+	public void setRegistry(UserListBean registry) {
+		this.registry = registry;
+	}
+
 	// Helper methods for notification   
     private void error(String message) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", message));
