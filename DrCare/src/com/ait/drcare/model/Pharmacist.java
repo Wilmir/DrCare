ae8c30310604ae7effe87f16fe402796370dfe18
Peter Casey <A00278938@student.ait.ie>
2020-08-28 17:38:52 +0100

    Pharmacist test changes

--- DrCare/src/com/ait/drcare/model/Pharmacist.java
+++ DrCare/src/com/ait/drcare/model/Pharmacist.java
@@ -8,11 +8,12 @@ public class Pharmacist extends User{
 	//Orders currently assigned to be dispensed here
 	private ArrayList<Prescription> orders;
 
-	
+	private Prescription accessor = new Prescription();
 	
 	public Pharmacist()
 	{
 		this.role = "Pharmacist";
+		this.orders = new ArrayList<Prescription>();
 	}
 	
 	public Pharmacist(String name, int ID, String password,
@@ -20,33 +21,44 @@ public class Pharmacist extends User{
 		//Send to parent class constructor
 		super(name, ID, password, email,address,"Pharmacist");
 		
+		this.orders = new ArrayList<Prescription>();
 	}
 
-	public ArrayList<Prescription> getOrders() {
-		return orders;
-	}
+	
 
 	public void AddOrder(Prescription order){
 		orders.add(order);
 		//Update display if needed
 	}
 
-	public void UpdateOrder(Prescription order) {
+	public String orderReadyForPickup(Prescription order) {
 		
-		
-		
-		
+		order.setTheStatus(order.PRESCRIPTION_READY_FOR_PICKUP);
+		return order.getTheStatus();
 	}
 	
-	public void DispenseOrder() {
-		//Get order, change status to DISPENSED
+	public String orderDispensed(Prescription order) {
+		order.setTheStatus(order.PRESCRIPTION_DISPENSED);
 		
+		return order.getTheStatus();
 	}
+	
+	
 
+
+	public String removeOrder(Prescription order) {
+		orders.remove(order);
+		
+		return null;
+	}
+	
 	//Intended for use of database
 	public void setOrders(ArrayList<Prescription> orders) {
 		this.orders = orders;
 	}
 	
+	public ArrayList<Prescription> getOrders() {
+		return orders;
+	}
 	
 }
