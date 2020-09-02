add844c5a26257509925dd6c608e9cd48949322d
Wilmir <a00278899@student.ait.ie>
2020-09-02 16:16:55 +0100

    Adds predefined values for contents and side effects of medicines

--- DrCare/src/com/ait/drcare/managedbeans/support/MedicineListBean.java
+++ DrCare/src/com/ait/drcare/managedbeans/support/MedicineListBean.java
@@ -1,15 +1,11 @@
 package com.ait.drcare.managedbeans.support;
 
 
-import java.sql.Date;
 import java.util.ArrayList;
-
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
 import com.ait.drcare.model.Medicine;
-
 
 
 @ManagedBean
@@ -17,34 +13,43 @@ import com.ait.drcare.model.Medicine;
 public class MedicineListBean {
 	
 	public ArrayList<Medicine> medications;
-	
 	private double theDosage = 20;
 	private int theDays = 20;
 	private int testDay = 2;
 	
-	
-	
-
 	@PostConstruct
 	public void init() {
+		medications  = new ArrayList<Medicine>();
+		theDosage  = 10;
+		theDays = 1;
 		
-	medications  = new ArrayList<Medicine>();
-	theDosage  = 10;
-	theDays = 1;
+		medications.add(new Medicine("Medicine", 2.00));
+		medications.add(new Medicine("Bean Juice", 3.50));
+		medications.add(new Medicine("Beanies Sip", 5.20));
+		medications.add(new Medicine("Bean", 4.25));
+		medications.add(new Medicine("Lrgal", 2.25));
+		
+		ArrayList<String> ingredients = new ArrayList<>();
+			ingredients.add("Sugar");
+			ingredients.add("Spice");
+			ingredients.add("And everything nice");
+
+		
+		ArrayList<String> sideEffects = new ArrayList<>();
+			sideEffects.add("Constipation");
+			sideEffects.add("Dizziness");
+			sideEffects.add("Dry mouth");
+			
+			
+		for(Medicine medicine : medications) {
+			medicine.setTheContents(ingredients);
+			medicine.setTheSideEffects(sideEffects);
+		}	
 	}
 	
 	
-	public ArrayList<Medicine> getMedicineList(){
-		
-		ArrayList<Medicine> meds  = new ArrayList<Medicine>();
-		meds.add(new Medicine("Medicine"));
-		meds.add(new Medicine("Bean Juice"));
-		meds.add(new Medicine("Beanies Sip"));
-		meds.add(new Medicine("Bean"));
-		meds.add(new Medicine("Lrgal"));
-		
-		return meds;
-		
+	public ArrayList<Medicine> getMedicineList(){	
+		return medications;
 	}
 	
 	public double getDosage() {
@@ -58,7 +63,15 @@ public class MedicineListBean {
 		return testDay;
 	}
 
-	
-	
+
+	public ArrayList<Medicine> getMedications() {
+		return medications;
+	}
+
+
+	public void setMedications(ArrayList<Medicine> medications) {
+		this.medications = medications;
+	}
+
 }
 
