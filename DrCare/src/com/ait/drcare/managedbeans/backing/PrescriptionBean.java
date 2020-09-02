34fbc9fcf2a6f90bb2ea970c87e1214b0dec5238
Wilmir <a00278899@student.ait.ie>
2020-09-02 16:32:09 +0100

    Created the prescription bean and added a placeholder attribute

---
+++ DrCare/src/com/ait/drcare/managedbeans/backing/PrescriptionBean.java
@@ -0,0 +1,24 @@
+package com.ait.drcare.managedbeans.backing;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PrescriptionBean {
+
+	// This variable is only used as a placeholder when developing the frontEnd
+	// Remove this and its getters and setters once you are done developing the PrescriptionBean
+	private Object placeholder;
+
+	public Object getPlaceholder() {
+		return placeholder;
+	}
+
+	public void setPlaceholder(Object placeholder) {
+		this.placeholder = placeholder;
+	}
+	
+	
+	
+}
