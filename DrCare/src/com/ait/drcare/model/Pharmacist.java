04de966fc7902a5102705c80783dff8c6feb194f
Wilmir <a00278899@student.ait.ie>
2020-09-10 18:29:10 +0100

    Places the greetings next to the logout button

--- DrCare/src/com/ait/drcare/model/Pharmacist.java
+++ DrCare/src/com/ait/drcare/model/Pharmacist.java
@@ -15,17 +15,5 @@ public class Pharmacist extends User{
 		super(name, ID, password, email,address,"Pharmacist");
 	}
 
-	public String orderReadyForPickup(Prescription order) {
-		
-		order.setTheStatus(order.PRESCRIPTION_READY_FOR_PICKUP);
-		return order.getTheStatus();
-	}
-	
-
-	public String orderDispensed(Prescription order) {
-		order.setTheStatus(order.PRESCRIPTION_DISPENSED);
-		return null;
-	}
-	
 	
 }
