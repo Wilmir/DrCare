5a924621ee5873a71c1c1285dab0c1f410bf5e1a
Wilmir <a00278899@student.ait.ie>
2020-09-14 13:39:20 +0100

    Show list of patients in the Doctor page

--- DrCare/src/com/ait/drcare/managedbeans/backing/DoctorBean.java
+++ DrCare/src/com/ait/drcare/managedbeans/backing/DoctorBean.java
@@ -2,38 +2,38 @@ package com.ait.drcare.managedbeans.backing;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import com.ait.drcare.helpers.Helper;
 import com.ait.drcare.managedbeans.support.UserListBean;
 import com.ait.drcare.model.Doctor;
 import com.ait.drcare.model.Patient;
+import com.ait.drcare.model.Prescription;
 import com.ait.drcare.model.User;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class DoctorBean {
 	private Doctor doctor;
 	private Patient currentPatient;
+	private Prescription currentPrescription;
 	private UserListBean existingUsers;
 	
 	@PostConstruct
 	public void init() {
-		 existingUsers = Helper.getBean("userListBean", UserListBean.class);
-		
+		 existingUsers = Helper.getBean("userListBean", UserListBean.class);		
+	}
+
+	public Doctor getDoctor() {
 		String email = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
-				
+		
 		for(User user: existingUsers.getUsers()) {
 			if(user.getTheEmail().equalsIgnoreCase(email)) {
 				this.doctor = (Doctor) user;
 				break;
 			}
 		}
-		
-	}
-
-	public Doctor getDoctor() {
 		return doctor;
 	}
 
@@ -49,4 +49,12 @@ public class DoctorBean {
 		this.currentPatient = currentPatient;
 	}
 
+	public Prescription getCurrentPrescription() {
+		return currentPrescription;
+	}
+
+	public void setCurrentPrescription(Prescription currentPrescription) {
+		this.currentPrescription = currentPrescription;
+	}
+	
 }
