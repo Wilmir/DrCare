57545b42a8cc98c094c1a54e1d3894fb4a61fd91
Peter Casey <A00278938@student.ait.ie>
2020-09-15 12:14:24 +0100

    PharmacistBean Partially 50% tested

--- DrCare/tests/com/ait/drcare/managedbeans/backing/test/PharmacistBeanTest.java
+++ DrCare/tests/com/ait/drcare/managedbeans/backing/test/PharmacistBeanTest.java
@@ -2,6 +2,8 @@ package com.ait.drcare.managedbeans.backing.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ import com.ait.drcare.managedbeans.support.UserListBean;
 import com.ait.drcare.model.Pharmacist;
 import com.ait.drcare.model.Prescription;
 import com.ait.drcare.model.User;
+import com.ait.drcare.model.Video;
 
 class PharmacistBeanTest {
 
@@ -33,16 +36,53 @@ class PharmacistBeanTest {
 	}
 	
 	@Test
-	void testCurrentPrescription() {
+	void testCurrentPrescriptionStatus() {
 		Prescription prescription = new Prescription();
 		prescription.setTheStatus(Prescription.PRESCRIPTION_READY_FOR_PICKUP);
 		
 		pharmacistBean.setCurrentPrescription(prescription);
 		
 		assertEquals("Ready for pickup", pharmacistBean.getCurrentPrescription().getTheStatus());
+		assertTrue(pharmacistBean.isPreview());
+		pharmacistBean.setPreview(false);
 		assertFalse(pharmacistBean.isPreview());
+		pharmacistBean.preview();
+		assertTrue(pharmacistBean.isPreview());
 	}
 	
+	@Test
+	void testVideoRetreival() {
+		Video v = new Video("Video", "whoop");
+		pharmacistBean.setVideo(v);
+		assertEquals(v,pharmacistBean.getVideo());
+	}
 	
+	@Test
+	void testPrescriptionSettersGetters() {
+		Prescription prescription = new Prescription();
+		Prescription prescription1 = new Prescription();
+		ArrayList<Prescription> prescriptions = new ArrayList<Prescription>();
+		prescriptions.add(prescription1);
+		pharmacistBean.setPaidPrescriptions(prescriptions);
+		
+		assertEquals(prescriptions, pharmacistBean.getPaidPrescriptions());
+		assertEquals(1,pharmacistBean.getQueueSize());
+		pharmacistBean.setQueueSize(2);
+		
+		
+	}
+	
+	@Test 
+	void testnextPrescription() {
+		Prescription prescription = new Prescription();
+		Prescription prescription1 = new Prescription();
+		ArrayList<Prescription> prescriptions = new ArrayList<Prescription>();
+		prescriptions.add(prescription1);
+		prescriptions.add(prescription);
+		pharmacistBean.setPaidPrescriptions(prescriptions);
+		Pharmacist pharmacist = new Pharmacist("Wilmir Nicanor", 30000001, "password", "wilmir@google.com", "Athlone, Ireland");
+		pharmacistBean.setCurrentUser(pharmacist);
+		
+	}
 	
 }
